12b92cb8035185682de0c365790cdfa7e464d62f
Claudia Stevens <71283725+claudiajstevens@users.noreply.github.com>
2022-08-24 18:11:35 +0000

    URL shortener satisfies all user stories.

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+//added
+const bodyParser = require('body-parser');
+global.longURLs = [];
+var dns = require('dns');
+
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -19,6 +25,45 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+//You can POST a URL to /api/shorturl and get a JSON response with original_url and short_url properties. Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
+//When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
+app.post("/api/shorturl", function(req, res){
+  let longURL = req.body.url;
+  const urlObj = new URL(longURL);
+  let myJSON = {"original_url":"", "short_url":""};
+  
+
+  let validated = dns.lookup(urlObj.hostname, (err, addresses, family) => {
+    if(err){
+      myJSON = {
+        error: 'invalid url'
+      }
+    }else{
+      longURLs.push(longURL);
+      myJSON.original_url = longURL;
+      myJSON.short_url = longURLs.indexOf(longURL);
+    }
+    res.json(myJSON);
+  });
+  
+});
+
+//When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
+app.get("/api/shorturl/:url", function(req, res){
+  console.log(req.params.url);
+  console.log(longURLs[req.params.url]);
+  res.writeHead(307, {
+    Location: longURLs[req.params.url]
+  }).end();
+})
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+
+
+
